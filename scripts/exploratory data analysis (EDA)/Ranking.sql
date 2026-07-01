b8268384c595d79c7a15385df8f0fceb293eb12a
Tom0809 <114962033+Tom0809@users.noreply.github.com>
2026-06-30 23:33:33 -0700

    Add Ranking SQL queries for EDA
    Added SQL queries for ranking products and customers based on sales performance.

---
+++ scripts/exploratory data analysis (EDA)/Ranking.sql
@@ -0,0 +1,37 @@
+-- Ranking (Order values of dimensions by measure, Top N performers | bottom N  performers)
+-- Rank [Dimension] By [Measure], ex. Rank countries by total sales
+
+-- Which 5 products generate the highest revenue?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- What are 5 worst-performing products in terms of sales?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue;
+
+-- The 3 customers with the fewest orders placed
+SELECT TOP 3
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_orders;
